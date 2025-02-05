31b99e409273f16c3209c7d47e2a26c36ae0e5e9
AlumG <AlumG@majada.es>
2025-02-05 17:05:42 +0100

    fresh react@vite install

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-3xl font-bold underline flex justify-center items-center'>
+        <p>Hello</p>
+      </div>
+    </>
+  )
+}
+
+export default App
