03010643a29a4017b99952cc1d33d233d91dce03
gastbr <71907959+gastbr@users.noreply.github.com>
2025-02-09 09:44:13 +0000

    Set up for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/DAW.DEW--react-calendar/',
   plugins: [
     react(),
     tailwindcss()
