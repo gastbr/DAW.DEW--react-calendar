966e65881d138a7dbf9cc78c9df999a8e75bdd8e
gastbr <71907959+gastbr@users.noreply.github.com>
2025-02-09 02:20:58 +0000

    popup height fix

---
+++ src/components/eventContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useReducer, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+const EventContext = createContext();
+
+const eventReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_EVENT':
+            return [...state, action.payload];
+        case 'EDIT_EVENT':
+            return state.map((event) =>
+                event.id === action.payload.id ? action.payload : event
+            );
+        case 'DELETE_EVENT':
+            return state.filter((event) => event.id !== action.payload);
+        case 'SET_EVENTS':
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+const EventProvider = ({ children }) => {
+
+    EventProvider.propTypes = {
+        // Prop types validation
+        children: PropTypes.array.isRequired,
+    }
+
+    const [events, dispatch] = useReducer(eventReducer, [], () => {
+        const localData = localStorage.getItem('events');
+        return localData ? JSON.parse(localData) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem('events', JSON.stringify(events));
+    }, [events]);
+
+    return (
+        <EventContext.Provider value={{ events, dispatch }}>
+            {children}
+        </EventContext.Provider>
+    );
+};
+
+export { EventContext, EventProvider };
